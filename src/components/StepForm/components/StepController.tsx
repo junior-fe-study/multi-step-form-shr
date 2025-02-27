1407924b8258de2872ade2433b1679140e1cf10d
rachel490 <ccori12@naver.com>
2025-02-27 17:19:50 +0900

    feat: add step validation

--- src/components/StepForm/components/StepController.tsx
+++ src/components/StepForm/components/StepController.tsx
@@ -1,3 +1,5 @@
+import { useFormContext } from 'react-hook-form';
+import { StepFormSchemaType } from '../hooks/useStepForm';
 import { MAX_STEP, START_STEP } from '../constants/steps';
 
 function StepController({
@@ -7,18 +9,40 @@ function StepController({
   step: number;
   changeStep: (step: number) => void;
 }) {
+  const { watch } = useFormContext<StepFormSchemaType>();
+
   const handleConfirm = () => {
     console.log('confirm');
   };
 
+  const checkStepValidation = () => {
+    const { email, name, phone, plan, planPeriod, addons } = watch();
+
+    switch (step) {
+      case 1:
+        return !email || !name || !phone;
+      case 2:
+        return !plan || !planPeriod;
+      case 3:
+        return !addons;
+      case 4:
+        return false;
+    }
+  };
+
   return (
     <div className="flex justify-between">
-      <button
-        onClick={() => changeStep(step - 1)}
-        className="text-grey font-body-l"
-      >
-        {step === START_STEP ? '' : 'Go Back'}
-      </button>
+      {step !== START_STEP ? (
+        <button
+          onClick={() => changeStep(step - 1)}
+          className="text-grey font-body-l"
+        >
+          Go Back
+        </button>
+      ) : (
+        <div></div>
+      )}
+
       {step === MAX_STEP ? (
         <button
           onClick={handleConfirm}
@@ -29,7 +53,8 @@ function StepController({
       ) : (
         <button
           onClick={() => changeStep(step + 1)}
-          className="rounded-[8px] bg-denim px-[25px] h-[48px] flex items-center justify-center text-white"
+          disabled={checkStepValidation()}
+          className="rounded-[8px] bg-denim px-[25px] h-[48px] flex items-center justify-center text-white disabled:opacity-50 disabled:cursor-not-allowed"
         >
           Next Step
         </button>
